e429b2c4f024dbca267e3f9954c3e7eacfd7195a
Umair Q <thefaintedbrain@gmail.com>
2016-08-15 14:09:33 +0500

    Sendgrid API removed

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@ date_default_timezone_set('Asia/Karachi');
 define ('siteroot','http://localhost/fyp/');
 
 //API key for send grid
-define('sendgridapi', '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');
+define('sendgridapi', '#####################');
 
 
 //Database settings
